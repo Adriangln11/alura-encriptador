91bb958b04439babb06d823e99a0ca703a5a367f
AdrianDiaz <cesarqqu@gmail.com>
2023-04-12 21:44:41 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+const inputTextArea =  document.getElementById('inputTextArea')
+const outputTextArea =  document.getElementById('outputTextArea')
+const btnInputContainer = document.getElementById('btnInputContainer ')
+const btnOutputContainer = document.getElementById('btnOutputContainer ')
